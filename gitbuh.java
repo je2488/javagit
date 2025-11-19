02fcf1dfadd90ad8a92a72fe8abe69acdf8d1932
https://github.com/je2488/javagit.git <jenishshrestha0001@gmail.com>
2025-11-19 13:03:37 +0545

    mm

--- gitbuh.java
+++ gitbuh.java
@@ -7,9 +7,16 @@
  */
 public class gitbuh
 {
+    int age;
+    static int agr;
     public static void main(String[] args)
     
     {
+        int qty;
         System.out.print("its my third workshop");
+        gitbuh w1= new gitbuh();
+        System.out.println(w1.age);
+        System.out.println(agr);
+        
     }
 }
---
+++ mathoperator.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class mathoperator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class mathoperator
+{
+    public static void main(String[] args)
+    {}
+}
